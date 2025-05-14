3bf2877b5ef306b3a478782da8d08a6a03cf70b8
Paulo Daniel <paulotorresousa@gmail.com>
2025-05-14 05:34:34 -0300

    oi

--- index.cjs
+++ index.cjs
@@ -1,21 +1,51 @@
 const express = require("express");
-const cors = require("cors");
-const firebaseAdmin = require("firebase-admin");
-const { verifyTotp } = require("./totpRoutes");
-
 const app = express();
-app.use(cors());
+const speakeasy = require("speakeasy");
+const firebaseAdmin = require("firebase-admin");
+
 app.use(express.json());
 
-// Inicializando o Firebase Admin SDK
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./path/to/your/serviceAccountKey.json"); // Atualize o caminho
 
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
 });
 
-app.post("/verifyTotp", verifyTotp);
+app.post("/verifyTotp", (req, res) => {
+  const { token, userId } = req.body;
+
+  const db = firebaseAdmin.firestore();
+  const userRef = db.collection("users").doc(userId);
+
+  userRef
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).send("Usuário não encontrado");
+      }
+
+      const userData = doc.data();
+      const secret = userData.secret;
+
+      const verified = speakeasy.totp.verify({
+        secret,
+        encoding: "base32",
+        token,
+      });
+
+      if (verified) {
+        return res.status(200).send("Token verificado com sucesso");
+      } else {
+        return res.status(400).send("Token inválido");
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao verificar o TOTP:", error);
+      res.status(500).send("Erro no servidor");
+    });
+});
 
 app.listen(3000, () => {
-  console.log("Backend rodando na porta 3000");
+  console.log("Servidor rodando na porta 3000");
 });
+
