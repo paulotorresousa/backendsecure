80806e1b36a6b4b7b47af9759784b1516317e588
paulotorresousa <paulotorresousa@gmail.com>
2025-05-15 00:39:51 -0300

    Create firestore.py

---
+++ firestore.py
@@ -0,0 +1,10 @@
+from firebase_admin import firestore
+
+db = firestore.client()
+
+def set_totp_secret(uid, secret):
+    db.collection("users").document(uid).update({"totp_secret": secret})
+
+def get_totp_secret(uid):
+    doc = db.collection("users").document(uid).get()
+    return doc.to_dict().get("totp_secret") if doc.exists else None
