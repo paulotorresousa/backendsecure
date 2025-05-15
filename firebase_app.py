d9fb36addcee61c65ab20767e3503c57adc552a9
paulotorresousa <paulotorresousa@gmail.com>
2025-05-15 01:36:39 -0300

    Update firebase_app.py

--- firebase_app.py
+++ firebase_app.py
@@ -1,10 +1,12 @@
 # firebase_app.py
 import json
 import os
+import base64
 import firebase_admin
 from firebase_admin import credentials
 
 if not firebase_admin._apps:
-    cred_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    cred_json = os.environ.get("GOOGLE_CREDENTIALS_B64")
+    decoded_json = base64.b64decode(encoded).decode()
     cred = credentials.Certificate(json.loads(cred_json))
     firebase_admin.initialize_app(cred)
